dba9ad4baa4f20ebbcf462242a40cc5aa0493c6b
jophc1 <jphcairns@gmail.com>
2023-08-10 20:45:06 +1000

    adding get all reviews

---
+++ src/routes/review_routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express"
+import { ReviewModel } from "@src/models"
+
+const reviewRouter = Router()
+
+reviewRouter.get('/', async (req, res) => {
+    const reviews = await ReviewModel.find()
+    reviews ? res.send(reviews) : res.status(404).send({ error: "No reviews found"})
+})
